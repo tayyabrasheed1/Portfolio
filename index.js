7113ea7d3e2c2a82d0e1f5d0afbe0237c5e5aca4
Tayyab Rasheed <137378706+tayyabrasheed1@users.noreply.github.com>
2023-09-04 01:04:35 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        window.scrollTo({
+            top: targetElement.offsetTop - 20,
+            behavior: 'smooth'
+        });
+    });
+});
